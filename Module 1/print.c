7afc34383a36229dda76e616b4584654615fe6fc
MIhirDas10 <69119099+MIhirDas10@users.noreply.github.com>
2024-04-02 13:00:12 +0600

    Add files via upload

---
+++ Module 1/print.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // header file
+int main() // won't work without it
+{
+    printf("Hello World\n");
+    printf("\tHi, I am a programmer\n");
+    printf("Hi,\tthere\n");
+    printf("100%%");
+    return 0;
+}
+// ESCAPE CHARACTERS
+// \n --- new line
+// \t --- tab
